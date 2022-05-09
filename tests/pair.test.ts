f43f5cfb342cc1a7c4455ce813b775ae6d802988
Luke Shiru <hi@luke.sh>
2022-05-09 11:24:01 -0700

    :sparkles: Initial commit.

---
+++ tests/pair.test.ts
@@ -0,0 +1,58 @@
+import type { Tests } from "@vangware/test";
+import { h } from "preact";
+import renderToString from "preact-render-to-string";
+import { useState } from "preact/hooks";
+import { pair } from "../src/pair.js";
+import type { PairedRenderFunction } from "../src/PairedRenderFunction.js";
+
+const children = (usePairedState: typeof useState) => {
+	// eslint-disable-next-line react-hooks/rules-of-hooks
+	const [count, setCount] = usePairedState(0);
+
+	return h(
+		"button",
+		{ onClick: () => setCount(count + 1), type: "button" },
+		count,
+	);
+};
+
+const key = "TEST";
+
+const PairedState = pair(useState);
+
+export default [
+	{
+		given: "a paired hook with key",
+		must: "return component wrapping hook and with key",
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		received: renderToString(h(PairedState, { children, key })),
+		wanted: renderToString(
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			h(
+				({
+					children: render,
+				}: {
+					readonly children: PairedRenderFunction<typeof useState>;
+				}) => render(useState),
+				{ children, key },
+			),
+		),
+	},
+	{
+		given: "a paired hook without key",
+		must: "return component wrapping hook and without key",
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+		received: renderToString(h(PairedState, { children })),
+		wanted: renderToString(
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+			h(
+				({
+					children: render,
+				}: {
+					readonly children: PairedRenderFunction<typeof useState>;
+				}) => render(useState),
+				{ children },
+			),
+		),
+	},
+] as Tests;
