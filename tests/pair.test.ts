dc5cd3b7027a563c100798d2ae631eb8a28a43f1
Lou Cyx <git@lou.cx>
2022-08-22 01:46:43 -0700

    :arrow_up: dependency upgrade.

--- tests/pair.test.ts
+++ tests/pair.test.ts
@@ -1,16 +1,9 @@
 import type { Tests } from "@vangware/test";
 import { h } from "preact";
-// @ts-expect-error "preact-render-to-string" doesn't work well with the new TS types
-import preactRenderToString from "preact-render-to-string";
+import { renderToString } from "preact-render-to-string";
 import { useState } from "preact/hooks";
 import { pair } from "../src/pair.js";
 import type { PairedRenderFunction } from "../src/PairedRenderFunction.js";
-
-// TODO: Remove this once the types are fixed in Preact's side.
-// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-const renderToString = preactRenderToString as (
-	...stuff: ReadonlyArray<unknown>
-) => string;
 
 const children = (usePairedState: typeof useState) => {
 	// eslint-disable-next-line react-hooks/rules-of-hooks
@@ -32,34 +25,40 @@ export default [
 		given: "a paired hook with key",
 		must: "return component wrapping hook and with key",
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		received: renderToString(h(PairedState, { children, key })),
-		wanted: renderToString(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-			h(
-				({
-					children: render,
-				}: {
-					readonly children: PairedRenderFunction<typeof useState>;
-				}) => render(useState),
-				{ children, key },
+		received: () => renderToString(h(PairedState, { children, key })),
+		wanted: () =>
+			renderToString(
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+				h(
+					({
+						children: render,
+					}: {
+						readonly children: PairedRenderFunction<
+							typeof useState
+						>;
+					}) => render(useState),
+					{ children, key },
+				),
 			),
-		),
 	},
 	{
 		given: "a paired hook without key",
 		must: "return component wrapping hook and without key",
 		// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-		received: renderToString(h(PairedState, { children })),
-		wanted: renderToString(
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-			h(
-				({
-					children: render,
-				}: {
-					readonly children: PairedRenderFunction<typeof useState>;
-				}) => render(useState),
-				{ children },
+		received: () => renderToString(h(PairedState, { children })),
+		wanted: () =>
+			renderToString(
+				// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+				h(
+					({
+						children: render,
+					}: {
+						readonly children: PairedRenderFunction<
+							typeof useState
+						>;
+					}) => render(useState),
+					{ children },
+				),
 			),
-		),
 	},
 ] as Tests;
